b375a9f3e689b031525c72096aae52e3fb894275
juanlrcricci <juanjo.ricci@live.com.ar>
2020-10-26 17:45:37 -0300

    act6

--- test_suite.py
+++ test_suite.py
@@ -1,3 +1,18 @@
 import unittest
-from test_ahorcado import TestAhorcado
 from test_partida import TestPartida
+from test_ahorcado import TestAhorcado
+
+
+def suite():
+    """
+        Gather all the tests from this module in a test suite.
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(Test1))
+    test_suite.addTest(unittest.makeSuite(Test2))
+    return test_suite
+
+if __name__ == "__main__":
+    alltests = unittest.TestSuite()
+    alltests.addTest(suite())
+    unittest.TextTestRunner().run(alltests) 
