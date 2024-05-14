2cc6d565081db531e1a4a15d78c03f966d5f5893
Juan Ricci <j.ricci@alumno.um.edu.ar>
2024-05-14 18:20:11 -0300

    fixed One Player problem

--- repositorios.py
+++ repositorios.py
@@ -1,7 +1,7 @@
 class Repositorios:
     palabrasList = dict()
-    palabrasList[0] = {'palabra': 'palabra', 'tipo_palabra': 'tipo_palabra'}
-    palabrasList[1] = {'palabra': 'palabra1', 'tipo_palabra': 'tipo_palabra'}
-    palabrasList[2] = {'palabra': 'palabra2', 'tipo_palabra': 'tipo_palabra'}
-    palabrasList[3] = {'palabra': 'palabra3', 'tipo_palabra': 'tipo_palabra'}
+    palabrasList[0] = {'palabra': 'calabaza', 'tipo_palabra': 'comida'}
+    palabrasList[1] = {'palabra': 'portico', 'tipo_palabra': 'espacio arquitectónico'}
+    palabrasList[2] = {'palabra': 'esperanza', 'tipo_palabra': 'sentimiento'}
+    palabrasList[3] = {'palabra': 'atardecer', 'tipo_palabra': 'momento del día'}
     partida = dict()
